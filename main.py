ee622863c6dd08030824d7cfbcf155cefe3c5506
Arden Rasmussen <ardenisthebest@gmail.com>
2019-04-02 12:07:21 -0700

    Added main

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def main():
+    n_pop = 100
+    n_points = 10
+    n_keep = 10
+    prob_m = 0.2
+    pop = initalize(n_pop, n_points)
+    for gen in range(100):
+        fitness = evaluate(pop)
+        if terminate(fitness):
+            break
+        new_pop = selection(pop, fitness, n_keep)
+        new_pop = crossover(new_pop, k)
+        new_pop = mutate(new_pop, prob_m)
+        pop = replace(pop, fitness, new_pop, n_keep)
+    print(max(fitness))
--- src/std/std.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef AETAS_STD_HPP_
-#define AETAS_STD_HPP_
-
-#include "vector.hpp"
-
-#endif  // AETAS_STD_HPP_
