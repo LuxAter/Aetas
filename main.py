2783853787f2fc4e14a77bd5738210bbcb507e06
Arden Rasmussen <ardenisthebest@gmail.com>
2019-04-03 11:47:00 -0700

    Added selection

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ import random
 from pprint import pprint
 
 
-def initialize(n_pop, n_points):
-    return [np.random.rand(2 * n_points) for i in range(n_pop)]
-
-
 def sort_by_fitness(pop, fitness):
+    """Sorts population based by the value of fitness, meaning our most fit is
+    at the begining, and the least fit is at the end."""
     fitness, pop = zip(*sorted(zip(fitness, pop)))
     return pop, fitness
 
 
 def plot_best(generation, pop, fitness, plot_n=1):
+    """Givin the current generation, the population, and fitness. This will
+    plot the best plot_n from the population, and saves it in a file."""
     if not isdir("img"):
         return
     pop, fitness = sort_by_fitness(pop, fitness)
@@ -37,7 +37,13 @@ def plot_best(generation, pop, fitness, plot_n=1):
     fig.savefig("img/{}.png".format(generation), dpi=200)
 
 
+def initialize(n_pop, n_points):
+    return [np.random.rand(2 * n_points) for i in range(n_pop)]
+
+
 def evaluate(pop):
+    """Evaluates the fitness of the population, calculating the total path
+    distance for the specified chromosome."""
     return [(np.sqrt(ch[0]**2 + ch[1]**2) + np.sum([
         np.sqrt((ch[i] - ch[i + 2])**2 + (ch[i + 1] - ch[i + 3])**2)
         for i in range(0,
@@ -45,14 +51,38 @@ def evaluate(pop):
     ]) + np.sqrt((ch[-2] - 1)**2 + (ch[-1] - 1)**2))
             for ch in pop]
 
+
 def terminate(fitness, tolerance):
     """Terminate process if reach total distance withing tolerance level of
     error from a straight line --> sqrt(2).
     """
     for i in fitness:
-        if abs((2**(1.0/2)) - i) < tolerance:
+        if abs((2**(1.0 / 2)) - i) < tolerance:
             return True
     return False
+
+def selection(pop, fitness, n_keep):
+    """Selects len(pop)-n_keep from the population utilized the roulette wheel
+    selection algorithm. Each chromosome is given a probability proportional to
+    the inverse of its fitness. Then using those probabilities a new population
+    is selected."""
+    total_fit = sum([1.0/x for x in fitness])
+    prob = []
+    cumulative = 0.0
+    for f in fitness:
+        cumulative += ((1.0/f) / total_fit)
+        prob.append(cumulative)
+    new_pop = []
+    for i in range(len(pop) - n_keep):
+        r = np.random.rand()
+        j = 0
+        for j in range(len(prob)):
+            if prob[j] >= r:
+                break
+        new_pop.append(pop[j])
+    return new_pop
+
+
 
 def crossover(new_pop, k):
     """Performs 2-fold crossover on the new population.
@@ -62,14 +92,16 @@ def crossover(new_pop, k):
     2. Sort the points and perform crossover in their order for each fold
     """
     shuffle(new_pop)
-    for i in range(len(new_pop)//2):
+    for i in range(len(new_pop) // 2):
         points = random.sample(range(1, len(new_pop[i])), k)
         points.sort()
         for fold in range(k):
             x = points[fold]
-            tmp = new_pop[2*i][:x].copy()
-            new_pop[2*i][:x], new_pop[2*i + 1][:x]  = new_pop[2*i + 1][:x], tmp
+            tmp = new_pop[2 * i][:x].copy()
+            new_pop[2 * i][:x], new_pop[2 * i + 1][:x] = new_pop[2 * i +
+                                                                 1][:x], tmp
     return new_pop
+
 
 def main():
     n_pop = 100
