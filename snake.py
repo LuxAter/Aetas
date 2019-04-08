4828cc982003994f7b959d028359389e71cb8199
Arden Rasmussen <ardenisthebest@gmail.com>
2019-04-07 22:41:24 -0700

    Added argparse

--- snake.py
+++ snake.py
@@ -1,19 +1,25 @@
 from enum import Enum
+from argparse import ArgumentParser
 import numpy as np
+
 
 def valid(position, N):
     """Valid if the following things are not true:
     1. has not intersected with the edge, so no x or y = 0 or N
     2. has not intersected with itself, so no tuple is the same as another tuple
     """
-    if len([item for item in position[0] if item[0] == -1 or item[0] == N or item[1] == -1 or item[1] == N]) != 0:
+    if len([
+            item for item in position[0]
+            if item[0] == -1 or item[0] == N or item[1] == -1 or item[1] == N
+    ]) != 0:
         return False
     elif len(position[0]) != len(set(position[0])):
         return False
     return True
 
+
 def draw(position, N):
-    print("\u250f{}\u2513".format("\u2501" * (N*2)))
+    print("\u250f{}\u2513".format("\u2501" * (N * 2)))
     for i in range(N):
         row = ['  '] * N
         for seg in position[0]:
@@ -22,8 +28,8 @@ def draw(position, N):
         if position[1][1] == i:
             row[position[1][0]] = '\033[32m\u2588\u2588\033[0m'
         print("\u2503{}\u2503".format(''.join(row)))
-    print("\u2517{}\u251B".format("\u2501" * (N*2)))
-    print("\033[H",end='',flush=True)
+    print("\u2517{}\u251B".format("\u2501" * (N * 2)))
+    print("\033[H", end='', flush=True)
 
 
 def user_input(position):
@@ -56,4 +62,8 @@ def snake(N, get_move):
 
 if __name__ == "__main__":
     print("\033[2J\033[H")
-    snake(20, user_input)
+    parser = ArgumentParser()
+    parser.add_argument(
+        "N", nargs='?', type=int, default=20, help="Size of snake grid")
+    args = parser.parse_args()
+    snake(args.N, user_input)
